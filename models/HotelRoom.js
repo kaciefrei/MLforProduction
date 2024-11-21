b6ca53438dac71601922ff972fd2051648243dc6
kaciefrei <kaci.bouaouli@efrei.net>
2024-11-21 11:18:50 +0100

    test apres modif sur dockerhub

--- models/HotelRoom.js
+++ models/HotelRoom.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 // Connect to MongoDB
-const dbURI = "mongodb://localhost:27017";
+const dbURI = "mongodb://mongo:27017";
 
 mongoose.connect(dbURI)
     .then(() => console.log('Connected to MongoDB'))
